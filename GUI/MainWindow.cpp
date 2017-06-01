6fdc92b3621b4812dcc72ae8bb98a7dd0138fcf6
Bako Bencze <bakobence@yahoo.com>
2017-06-01 12:56:41 +0300

    fsadf

--- GUI/MainWindow.cpp
+++ GUI/MainWindow.cpp
@@ -2,50 +2,50 @@
 
 namespace cagd
 {
-    MainWindow::MainWindow(QWidget *parent): QMainWindow(parent)
-    {
-        setupUi(this);
+	MainWindow::MainWindow(QWidget *parent): QMainWindow(parent)
+	{
+		setupUi(this);
 
-    /*
+	/*
 
-      the structure of the main window's central widget
+	  the structure of the main window's central widget
 
-     *---------------------------------------------------*
-     |                 central widget                    |
-     |                                                   |
-     |  *---------------------------*-----------------*  |
-     |  |     rendering context     |   scroll area   |  |
-     |  |       OpenGL widget       | *-------------* |  |
-     |  |                           | | side widget | |  |
-     |  |                           | |             | |  |
-     |  |                           | |             | |  |
-     |  |                           | *-------------* |  |
-     |  *---------------------------*-----------------*  |
-     |                                                   |
-     *---------------------------------------------------*
+	 *---------------------------------------------------*
+	 |                 central widget                    |
+	 |                                                   |
+	 |  *---------------------------*-----------------*  |
+	 |  |     rendering context     |   scroll area   |  |
+	 |  |       OpenGL widget       | *-------------* |  |
+	 |  |                           | | side widget | |  |
+	 |  |                           | |             | |  |
+	 |  |                           | |             | |  |
+	 |  |                           | *-------------* |  |
+	 |  *---------------------------*-----------------*  |
+	 |                                                   |
+	 *---------------------------------------------------*
 
-    */
-        _side_widget = new SideWidget(this);
+	*/
+		_side_widget = new SideWidget(this);
 
-        _scroll_area = new QScrollArea(this);
-        _scroll_area->setWidget(_side_widget);
-        _scroll_area->setSizePolicy(_side_widget->sizePolicy());
-        _scroll_area->setVerticalScrollBarPolicy(Qt::ScrollBarAlwaysOn);
+		_scroll_area = new QScrollArea(this);
+		_scroll_area->setWidget(_side_widget);
+		_scroll_area->setSizePolicy(_side_widget->sizePolicy());
+		_scroll_area->setVerticalScrollBarPolicy(Qt::ScrollBarAlwaysOn);
 
-        _gl_widget = new GLWidget(this);
+		_gl_widget = new GLWidget(this);
 
-        centralWidget()->setLayout(new QHBoxLayout());
-        centralWidget()->layout()->addWidget(_gl_widget);
-        centralWidget()->layout()->addWidget(_scroll_area);
+		centralWidget()->setLayout(new QHBoxLayout());
+		centralWidget()->layout()->addWidget(_gl_widget);
+		centralWidget()->layout()->addWidget(_scroll_area);
 
-        // creating a signal slot mechanism between the rendering context and the side widget
-        connect(_side_widget->rotate_x_slider, SIGNAL(valueChanged(int)), _gl_widget, SLOT(set_angle_x(int)));
-        connect(_side_widget->rotate_y_slider, SIGNAL(valueChanged(int)), _gl_widget, SLOT(set_angle_y(int)));
-        connect(_side_widget->rotate_z_slider, SIGNAL(valueChanged(int)), _gl_widget, SLOT(set_angle_z(int)));
+		// creating a signal slot mechanism between the rendering context and the side widget
+		connect(_side_widget->rotate_x_slider, SIGNAL(valueChanged(int)), _gl_widget, SLOT(set_angle_x(int)));
+		connect(_side_widget->rotate_y_slider, SIGNAL(valueChanged(int)), _gl_widget, SLOT(set_angle_y(int)));
+		connect(_side_widget->rotate_z_slider, SIGNAL(valueChanged(int)), _gl_widget, SLOT(set_angle_z(int)));
 
-        connect(_side_widget->zoom_factor_spin_box, SIGNAL(valueChanged(double)), _gl_widget, SLOT(set_zoom_factor(double)));
+		connect(_side_widget->zoom_factor_spin_box, SIGNAL(valueChanged(double)), _gl_widget, SLOT(set_zoom_factor(double)));
 
-        connect(_side_widget->trans_x_spin_box, SIGNAL(valueChanged(double)), _gl_widget, SLOT(set_trans_x(double)));
+		connect(_side_widget->trans_x_spin_box, SIGNAL(valueChanged(double)), _gl_widget, SLOT(set_trans_x(double)));
 		connect(_side_widget->trans_y_spin_box, SIGNAL(valueChanged(double)), _gl_widget, SLOT(set_trans_y(double)));
 		connect(_side_widget->trans_z_spin_box, SIGNAL(valueChanged(double)), _gl_widget, SLOT(set_trans_z(double)));
 		connect(_side_widget->spinBox, SIGNAL(valueChanged(int)), _gl_widget, SLOT(setConstant(int)));
@@ -58,13 +58,17 @@ namespace cagd
 
 		connect(_side_widget->joinButton,SIGNAL(toggled(bool)),_gl_widget,SLOT(join()));
 		connect(_side_widget->directionComboBox,SIGNAL(currentIndexChanged(int)),_gl_widget,SLOT(setJoinDirection(int)));
-    }
 
-    //--------------------------------
-    // implementation of private slots
-    //--------------------------------
-    void MainWindow::on_action_Quit_triggered()
-    {
-        qApp->exit(0);
-    }
+		connect(_side_widget->newMeshXSpinBox,SIGNAL(valueChanged(int)),_gl_widget,SLOT(setX(int)));
+		connect(_side_widget->newMeshYSpinBox,SIGNAL(valueChanged(int)),_gl_widget,SLOT(setY(int)));
+		connect(_side_widget->newMeshButton,SIGNAL(clicked()),_gl_widget,SLOT(addNewMesh()));
+	}
+
+	//--------------------------------
+	// implementation of private slots
+	//--------------------------------
+	void MainWindow::on_action_Quit_triggered()
+	{
+		qApp->exit(0);
+	}
 }
